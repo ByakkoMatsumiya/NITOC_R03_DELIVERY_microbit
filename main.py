b0bc9e83a74055651932a2d83d3ac3c95b72d4a2
K-han_Udon-shijo <77813933+HanQ-KQ-Matsumiya@users.noreply.github.com>
2021-07-12 16:41:01 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.py
@@ -0,0 +1,47 @@
+basic.show_leds("""
+    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    """)
+basic.pause(100)
+basic.show_leds("""
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    """)
+basic.pause(100)
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    """)
+basic.pause(100)
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    """)
+basic.pause(100)
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # # # # #
+    """)
+basic.pause(100)
+
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.pause(100)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.pause(100)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,50 @@
+basic.showLeds(`
+    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # # # # #
+    `)
+basic.forever(function () {
+	
+})
