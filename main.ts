f51f80e0194fd285b32e055a79ab0bad0d6c4352
K-han_Udon-shijo <77813933+HanQ-KQ-Matsumiya@users.noreply.github.com>
2021-07-12 17:18:36 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,6 +11,8 @@ input.onButtonPressed(Button.B, function () {
     led.unplot(x, y)
     y += randint(-1, 1)
 })
+let iy = 0
+let ix = 0
 let y = 0
 let x = 0
 basic.showLeds(`
@@ -62,10 +64,14 @@ basic.showLeds(`
     `)
 x = 0
 y = 0
+loops.everyInterval(10000, function () {
+    led.unplot(ix, iy)
+    iy = randint(0, 4)
+    iy = randint(0, 4)
+    led.plotBrightness(ix, iy, 32)
+})
 loops.everyInterval(1, function () {
     led.plot(x, y)
-})
-basic.forever(function () {
     if (x < 0) {
         x = 0
     }
@@ -78,8 +84,22 @@ basic.forever(function () {
     if (y > 4) {
         y = 4
     }
-    if (led.point(4, 4)) {
-        basic.clearScreen()
+    if (x == ix && y == iy) {
+        led.unplot(x, y)
+        led.unplot(ix, iy)
+        x = 0
+        y = 0
+    }
+    if (led.point(4, 4) && led.pointBrightness(4, 4) == 255) {
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -96,5 +116,8 @@ basic.forever(function () {
         basic.pause(100)
         basic.showString("GOAL!!")
         basic.clearScreen()
+        basic.pause(1000)
+        x = 0
+        y = 0
     }
 })
