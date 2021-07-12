f807ecb957d272dab4fa92150f6e57ee802b3c90
K-han_Udon-shijo <77813933+HanQ-KQ-Matsumiya@users.noreply.github.com>
2021-07-12 16:52:35 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onPinPressed(TouchPin.P0, function () {
+    y += 1
+})
+input.onButtonPressed(Button.A, function () {
+    x += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    x = 2
+    y = 2
+})
+input.onButtonPressed(Button.B, function () {
+    x += 1
+})
+input.onPinPressed(TouchPin.P1, function () {
+    y += -1
+})
+let y = 0
+let x = 0
 basic.showLeds(`
     # # # # #
     . . . . .
@@ -25,14 +43,6 @@ basic.pause(100)
 basic.showLeds(`
     . . . . .
     . . . . .
-    # # # # #
-    . . . . .
-    . . . . .
-    `)
-basic.pause(100)
-basic.showLeds(`
-    . . . . .
-    . . . . .
     . . . . .
     # # # # #
     . . . . .
@@ -45,6 +55,16 @@ basic.showLeds(`
     . . . . .
     # # # # #
     `)
-basic.forever(function () {
-	
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+x = 2
+y = 2
+loops.everyInterval(100, function () {
+    led.plot(x, y)
 })
